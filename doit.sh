006825515ef4e7dcc8df99ad67da558544921aab
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-10-18 20:53:37 +0000

    missing doT

--- doit.sh
+++ doit.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 ./threadedStereo ng2_stereo.cfg ~/pose_est.data -f  /media/reef/r20070523_061146_ng2_13_legWestSouthEst/i20070523_061146_cv/ -n 2 --confply
-/genTex ng2_stereo.cfg ~/pose_est.data -f  /media/reef/r20070523_061146_ng2_13_legWestSouthEst/i20070523_061146_cv/
+./genTex ng2_stereo.cfg ~/pose_est.data -f  /media/reef/r20070523_061146_ng2_13_legWestSouthEst/i20070523_061146_cv/

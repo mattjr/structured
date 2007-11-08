6325f89c1385d1b1d734be49415a05b9b3020eaf
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-11-08 23:10:04 +0000

    lodgen ccleanup

--- lodgen.cpp
+++ lodgen.cpp
@@ -41,83 +41,31 @@ using namespace libsnapper;
 //
 // Command-line arguments
 //
-static string stereo_config_file_name;
-static string contents_file_name;
-static string dir_name;
-
-static bool no_simp=false;
 
 static bool have_max_mesh_count = false;
 static unsigned int max_mesh_count;
-static int num_threads=1;
-static bool display_debug_images = true;
-
-static bool compress_textures = false;
 
 static int lodNum=3;
-static string stereo_calib_file_name;
-
-
-static std::map<int,std::string> texture_file_names;
-
-static int tex_size=512;
-
-
-extern std::vector<GtsBBox *> bboxes_all;
 
 //
 // Parse command line arguments into global variables
 //
 static bool parse_args( int argc, char *argv[ ] )
 {
-  bool have_stereo_config_file_name = false;
+
   
    
   int i=1;
   while( i < argc )
     {
-      if( strcmp( argv[i], "-r" ) == 0 )
-	{
-	  if( i == argc-1 ) return false;
-	  tex_size = atoi( argv[i+1] );
-	  i+=2;
-	}
-      else if( strcmp( argv[i], "-f" ) == 0 )
-	{
-	  if( i == argc-1 ) return false;
-	  dir_name=string( argv[i+1]) ;
-	  i+=2;
-	}
-      else if( strcmp( argv[i], "--nosimp" ) == 0 )
-	{
-	  no_simp=true;
-	  i+=1;
-	}
-      else if( strcmp( argv[i], "--compress-tex" ) == 0 )
-	{
-	  compress_textures=true;
-	  i+=1;
-	}
-      else if( strcmp( argv[i], "-n" ) == 0 )
-	{
-	  if( i == argc-1 ) return false;
-	  have_max_mesh_count = true;
-	  max_mesh_count = atoi( argv[i+1] );
-	  i+=2;
-	}
-      else if( strcmp( argv[i], "-t" ) == 0 )
-      {
-         if( i == argc-1 ) return false;
-         num_threads = atoi( argv[i+1] );
-	 i+=2;
-      }
-      else if( !have_stereo_config_file_name )
-	{
-	  stereo_config_file_name = argv[i];
-	  have_stereo_config_file_name = true;
-	  i++;
-	}
-   
+      if( strcmp( argv[i], "-n" ) == 0 )
+        {
+          if( i == argc-1 ) return false;
+          have_max_mesh_count = true;
+          max_mesh_count = atoi( argv[i+1] );
+          i+=2;
+        }
+    
       else
 	{
 	  cerr << "Error - unknown parameter: " << argv[i] << endl;
@@ -128,7 +76,7 @@ static bool parse_args( int argc, char *argv[ ] )
 
 
 
-  return ( have_stereo_config_file_name);
+  return ( true);
 }
 osg::Node *create_paged_lod(osg::Node * model,vector<string> lod_file_names){
 
@@ -183,51 +131,7 @@ void genPagedLod(vector< osg::ref_ptr <osg::Group> > nodes, vector< vector<strin
   }
 
 }
-GNode *loadBBox(int num,std::map<int,GtsMatrix *> &gts_trans_map){
-  char conf_name[255];
-  
-  sprintf(conf_name,"mesh-agg/bbox-%08d.txt",num);
-  
-  FILE *bboxfp = fopen(conf_name,"r");
-  int count;
-  GNode *bboxTree=NULL;
-  GSList * bboxes = NULL;
-  if(bboxfp){
-    char name[255];
-    double x1,x2,y1,y2,z1,z2;
-    GtsMatrix *mtmp=gts_matrix_identity(NULL);
-    int eof0, eof1,eof2,frame_count=0;
-    while (eof0 != EOF && eof1 != EOF && eof2 != EOF){
-      
-      eof0 = fscanf(bboxfp,"%d %s %lf %lf %lf %lf %lf %lf" ,&count, name,
-	     &x1,&y1,&z1,&x2,&y2,&z2);
-      
-      for(int i=0; i < 4; i++)
-	for(int j=0; j < 4; j++)
-	 eof1 = fscanf(bboxfp," %lf",&mtmp[i][j]);
-     eof2 = fscanf(bboxfp,"\n");
-      
-     
-      
-      texture_file_names[count]=(name);
-      GtsBBox *bbox= gts_bbox_new(gts_bbox_class(),NULL,x1,y1,z1,x2,y2,z2);
-      bbox->bounded=(void *)count;
-      bboxes= g_slist_prepend (bboxes,bbox);
-      bboxes_all.push_back(bbox);
-      gts_trans_map[count]=gts_matrix_inverse(mtmp);
-      frame_count++;
-    }
-    gts_matrix_destroy(mtmp);
-    fclose(bboxfp);
 
-    bboxTree=gts_bb_tree_new(bboxes);
-    return bboxTree;
-  }
-    printf("No bbox file bailing...\n");
-    return NULL;
-
-
-}
 
 //
 // Display information on how to use this program
@@ -255,70 +159,7 @@ int main( int argc, char *argv[ ] )
       print_usage( );
       exit( 1 );
     }
-#ifdef _MCD_CHECK
-  FILE *fpm=fopen("memstat.txt","w");
-  _MCD_MemStatLog(fpm);
-#endif 
-  //
-  // Figure out the directory that contains the config file 
-  //
-  string config_dir_name;
-  int slash_pos = stereo_config_file_name.rfind( "/" );
-  if( slash_pos != -1 )
-    config_dir_name = stereo_config_file_name.substr( 0, slash_pos+1 );
 
-
-  //
-  // Open the config file
-  // Ensure the option to display the feature finding debug images is on.
-  //
-  Config_File *config_file;
-  try
-    {
-      config_file = new Config_File( stereo_config_file_name );
-    }
-  catch( string error )
-    {
-      cerr << "ERROR - " << error << endl;
-      exit( 1 );
-    }
-  config_file->set_value( "SKF_SHOW_DEBUG_IMAGES"    , display_debug_images );
-  config_file->set_value( "SCF_SHOW_DEBUG_IMAGES"    , display_debug_images );
-  config_file->set_value( "NCC_SCF_SHOW_DEBUG_IMAGES", display_debug_images );
-
-  
-
-
-  //
-  // Load the stereo camera calibration 
-  //
-  Stereo_Calib *calib = NULL;
-  bool have_stereo_calib = false;
-   
-  if( config_file->get_value( "STEREO_CALIB_FILE", stereo_calib_file_name) )
-    {
-      stereo_calib_file_name = config_dir_name+stereo_calib_file_name;
-      try
-	{
-	  calib = new Stereo_Calib( stereo_calib_file_name );
-	}
-      catch( string error )
-	{
-	  cerr << "ERROR - " << error << endl;
-	  exit( 1 );
-	}
-      have_stereo_calib = true;
-    }      
-
- 
- 
-  
-  auv_data_tools::makedir("mesh");
-  if(num_threads > 1)
-    g_thread_init (NULL);
-  
-  std::map<int,GtsMatrix *> gts_trans_map;
-  
   string dicefile("mesh-agg/diced.txt");
   std::vector<string> meshNames;
   std::vector<vector<string > > outNames;

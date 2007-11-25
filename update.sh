bdac8ef346ab3202621afef857a0c956f61120fe
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-11-25 02:41:46 +0000

    dir permissions

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd ~/cvs/threadedStereo/
 cvs upd
+make
 
---
+++ updateall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ssh timesync cvs/threadedStereo/update.sh
+ssh coral cvs/threadedStereo/update.sh
+ssh int-70 cvs/threadedStereo/update.sh
+
+

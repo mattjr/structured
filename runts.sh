3d3308d25591d769649c087ea3adfb180f01953e
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-11-26 04:52:04 +0000

    lodgen in tscript

--- runts.sh
+++ runts.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 COUNT=0
+LOGDIR="/mnt/shared/log-tex"
+BASEPATH="/home/mkj/cvs/threadedStereo/"
+SUBVOLDIR="/mnt/shared/svol"
+SHAREBASE="/mnt/shared/"
+
 NUMPOSE=$((`grep PR_ \$1 |wc -l  | awk '{ print $1 }'` - 1))
 # bash until loop
 LAST=0
@@ -12,14 +17,14 @@ rm -f tscmds
 while [ $COUNT -lt $NUMPOSE ]; do
 check=$(($COUNT % $2));   
     if [ $check -eq 0 ]  && [ "$COUNT" -gt 0 ];then
-        echo "cd $3;/home/mkj/cvs/threadedStereo/threadedStereo /home/mkj/cvs/threadedStereo/ng2_stereo.cfg -f /media//coral/r20071004_013437_gbr_05_noggin_grids/i20071004_013437_cv/ $1 --single-run $LAST $COUNT" >> tscmds
+        echo "cd $3;$BASEPATH/threadedStereo $BASEPATH/ng2_stereo.cfg -f /media//coral/r20071004_013437_gbr_05_noggin_grids/i20071004_013437_cv/ $1 --single-run $LAST $COUNT" >> tscmds
 	LAST=$COUNT
     fi
         let COUNT=COUNT+1
 done
 if [ $check -gt 0 ];then
     final=$COUNT
-    echo "cd $3;/home/mkj/cvs/threadedStereo/threadedStereo /home/mkj/cvs/threadedStereo/ng2_stereo.cfg -f /media//coral/r20071004_013437_gbr_05_noggin_grids/i20071004_013437_cv/ $1 --single-run $LAST $final" >> tscmds
+    echo "cd $3;$BASEPATH/threadedStereo $BASEPATH/ng2_stereo.cfg -f /media//coral/r20071004_013437_gbr_05_noggin_grids/i20071004_013437_cv/ $1 --single-run $LAST $final" >> tscmds
 fi
 ~/cvs/threadedStereo/vrip/bin/loadbalance ~/loadlimit tscmds -logdir /mnt/shared/log-ts/  -noxload
 TARGET_DIR=/mnt/shared/svol
@@ -31,16 +36,13 @@ done
 #bash tscmds
 find /mnt/shared/svol -name 'surface-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.033 1" }' > /mnt/shared/svol/surface.conf
 find /mnt/shared/svol -name 'mb-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.1 0" }' >> /mnt/shared/svol/surface.conf
-LOGDIR="/mnt/shared/log-tex"
-BASEPATH="/home/mkj/cvs/threadedStereo/"
-SUBVOLDIR="/mnt/shared/svol"
-SHAREBASE="/mnt/shared/"
+
 echo -e "#!/bin/bash\nOUTDIR=\$PWD\nVRIP_HOME=$BASEPATH/vrip\nexport VRIP_DIR=\$VRIP_HOME/src/vrip/\nPATH=\$PATH:\$VRIP_HOME/bin\ncd $SUBVOLDIR\n" > runvrip.sh
 
 echo -e "$BASEPATH/vrip/bin/pvrip1 auto.vri \$OUTDIR/mesh-agg/total.ply surface.conf surface.conf  0.033 100M ~/loadlimit -logdir /mnt/shared/log -rampscale 300 -subvoldir $SUBVOLDIR -nocrunch -passtovrip -use_bigger_bbox\n" >> runvrip.sh
 chmod 0777  runvrip.sh 
 ./runvrip.sh
-STEREOCONFIG=/home/mkj/cvs/threadedStereo/ng2_stereo.cfg
+STEREOCONFIG=$BASEPATH/ng2_stereo.cfg
 IMGDIR=/media//coral/r20071004_013437_gbr_05_noggin_grids/i20071004_013437_cv/
 
 SUBVOL=25;
@@ -55,3 +57,6 @@ echo -e "cd $SUBVOLDIR\n$BASEPATH/vrip/bin/vripdicebbox surface.conf \$DICEDIR\n
 echo -e "cd \$DICEDIR\n$BASEPATH/vrip/bin/loadbalance ~/loadlimit gentexcmds -logdir $LOGDIR\n" >> diced.sh
 chmod 0777 diced.sh 
 ./diced.sh
+echo -e "#!/bin/bash\necho 'LODGen...\\n'\ncd $3;$BASEPATH/lodgen $3\n" > lodgen.sh
+chmod 0777 lodgen.sh
+./lodgen.sh

90d0afd2295abdff943b4309335f2eaba7361358
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-11-26 03:43:29 +0000

    script now with mb

--- runts.sh
+++ runts.sh
@@ -3,10 +3,10 @@ COUNT=0
 NUMPOSE=$((`grep PR_ \$1 |wc -l  | awk '{ print $1 }'` - 1))
 # bash until loop
 LAST=0
-if [ $# -eq 4 ];then
-    NUMPOSE=$4
-
+if [ $# -eq 5 ];then
+    NUMPOSE=$5
 fi
+
 echo "Creating Stereo Meshes for $NUMPOSE frame pairs"
 rm -f tscmds
 while [ $COUNT -lt $NUMPOSE ]; do
@@ -21,7 +21,15 @@ if [ $check -gt 0 ];then
     final=$COUNT
     echo "cd $3;/home/mkj/cvs/threadedStereo/threadedStereo /home/mkj/cvs/threadedStereo/ng2_stereo.cfg -f /media//coral/r20071004_013437_gbr_05_noggin_grids/i20071004_013437_cv/ $1 --single-run $LAST $final" >> tscmds
 fi
+
+
 ~/cvs/threadedStereo/vrip/bin/loadbalance ~/loadlimit tscmds -logdir /mnt/shared/log-ts/  -noxload
+TARGET_DIR=/mnt/shared/svol
+for i in `find $4 -name '*.ply'`; do
+    cp "$i" $TARGET_DIR/
+    echo  cp "$i" $TARGET_DIR/
+done
+
 #bash tscmds
 find /mnt/shared/svol -name 'surface-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.033 1" }' > /mnt/shared/svol/surface.conf
 find /mnt/shared/svol -name 'mb-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.1 0" }' >> /mnt/shared/svol/surface.conf

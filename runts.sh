e9aeceeca643bd2137c0ab86eab43f486ab59844
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-12-04 02:55:18 +0000

    runts to work with new args struct

--- runts.sh
+++ runts.sh
@@ -2,11 +2,12 @@
 COUNT=0
 LOGDIR="/mnt/shared/log-tex"
 BASEPATH=`dirname $0`
-SUBVOLDIR="/mnt/shared/svol"
+SUBVOLDIR="/mnt/shared/mesh-agg"
 SHAREBASE="/mnt/shared/"
 POSEFILE=$1/pose_est.data
 IMGDIR=$1/img/
 STEREOCONFIG=$1/stereo.cfg
+DATADIR=$1
 MBDIR="."
 OUTPUTLOC="/mnt/shared/"
 NUMPOSE=$((`grep PR_ \$1 |wc -l $POSEFILE | awk '{ print $1 }'` - 1))
@@ -22,25 +23,25 @@ rm -f tscmds
 while [ $COUNT -lt $NUMPOSE ]; do
 check=$(($COUNT % $SPLIT));   
     if [ $check -eq 0 ]  && [ "$COUNT" -gt 0 ];then
-        echo "cd $OUTPUTLOC;$BASEPATH/threadedStereo $STEREOCONFIG -f $IMGDIR $POSEFILE --single-run $LAST $COUNT" >> tscmds
+        echo "cd $OUTPUTLOC;$BASEPATH/threadedStereo $DATADIR --single-run $LAST $COUNT" >> tscmds
 	LAST=$COUNT
     fi
         let COUNT=COUNT+1
 done
 if [ $check -gt 0 ];then
     final=$COUNT
-    echo "cd $OUTPUTLOC;$BASEPATH/threadedStereo $STEREOCONFIG -f  $IMGDIR $POSEFILE --single-run $LAST $final" >> tscmds
+    echo "cd $OUTPUTLOC;$BASEPATH/threadedStereo $DATADIR --single-run $LAST $final" >> tscmds
 fi
 ~/cvs/threadedStereo/vrip/bin/loadbalance ~/loadlimit tscmds -logdir /mnt/shared/log-ts/  -noxload
-TARGET_DIR=/mnt/shared/svol
-for i in `find $MBDIR -name '*.ply'`; do
+TARGET_DIR=/mnt/shared/mesh-agg
+for i in `ls $MBDIR/mb*.ply`; do
     cp "$i" $TARGET_DIR/
     echo  cp "$i" $TARGET_DIR/
 done
 
 #bash tscmds
-find /mnt/shared/svol -name 'surface-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.033 1" }' > /mnt/shared/svol/surface.conf
-find /mnt/shared/svol -name 'mb-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.1 0" }' >> /mnt/shared/svol/surface.conf
+find /mnt/shared/mesh-agg -name 'surface-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.033 1" }' > /mnt/shared/mesh-agg/surface.conf
+find /mnt/shared/mesh-agg -name 'mb-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.1 0" }' >> /mnt/shared/mesh-agg/surface.conf
 
 echo -e "#!/bin/bash\nOUTDIR=\$PWD\nVRIP_HOME=$BASEPATH/vrip\nexport VRIP_DIR=\$VRIP_HOME/src/vrip/\nPATH=\$PATH:\$VRIP_HOME/bin\ncd $SUBVOLDIR\n" > runvrip.sh
 

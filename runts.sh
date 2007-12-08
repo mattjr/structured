c117ac03a8546d3bc9c1f0813149d9b4f5e9a0e0
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2007-12-08 01:44:48 +0000

    added ability to specifiy mesh cache diR

--- runts.sh
+++ runts.sh
@@ -3,20 +3,30 @@ COUNT=0
 LOGDIR="/mnt/shared/log-tex"
 BASEPATH=`dirname $0`
 SUBVOLDIR="/mnt/shared/mesh-agg"
+MESHCACHE=$1/cache-mesh/
 SHAREBASE="/mnt/shared/"
 POSEFILE=$1/pose_est.data
 IMGDIR=$1/img/
+CACHEDIMGDIR=$1/cache-tex/
 STEREOCONFIG=$1/stereo.cfg
 DATADIR=$1
 MBDIR="."
 OUTPUTLOC="/mnt/shared/"
 NUMPOSE=$((`grep PR_ \$1 |wc -l $POSEFILE | awk '{ print $1 }'` - 1))
+mkdir -p $PWD/mesh-agg/
+mkdir -p $PWD/mesh/
+chown 0777  $PWD/mesh-agg/
+chown 0777  $PWD/mesh/
+
 # bash until loop
 LAST=0
-if [ $# -eq 2 ];then
+if [ $# -gt 1 ] && [ $2 -ne 0 ]; then
     NUMPOSE=$2
 fi
-SPLIT=500
+
+if [ $# -gt 2 ]; then
+    SPLIT=$3
+fi
 
 echo "Creating Stereo Meshes for $NUMPOSE frame pairs"
 rm -f tscmds
@@ -32,7 +42,7 @@ if [ $check -gt 0 ];then
     final=$COUNT
     echo "cd $OUTPUTLOC;$BASEPATH/threadedStereo $DATADIR --single-run $LAST $final" >> tscmds
 fi
-~/cvs/threadedStereo/vrip/bin/loadbalance ~/loadlimit tscmds -logdir /mnt/shared/log-ts/  -noxload
+#~/cvs/threadedStereo/vrip/bin/loadbalance ~/loadlimit tscmds -logdir /mnt/shared/log-ts/  -noxload
 TARGET_DIR=/mnt/shared/mesh-agg
 for i in `ls $MBDIR/mb*.ply`; do
     cp "$i" $TARGET_DIR/
@@ -40,12 +50,12 @@ for i in `ls $MBDIR/mb*.ply`; do
 done
 
 #bash tscmds
-find /mnt/shared/mesh-agg -name 'surface-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.033 1" }' > /mnt/shared/mesh-agg/surface.conf
-find /mnt/shared/mesh-agg -name 'mb-*.ply' | sort  |  sed 's_.*/__' | awk '{print "bmesh " $0  " 0.1 0" }' >> /mnt/shared/mesh-agg/surface.conf
+find /mnt/shared/mesh-agg -name 'surface-*.ply' | sort  |  sed 's_.*/__' | awk '{print $0  " 0.033 1" }' > /mnt/shared/mesh-agg/surface.txt
+find /mnt/shared/mesh-agg -name 'mb-*.ply' | sort  |  sed 's_.*/__' | awk '{print $0  " 0.1 0" }' >> /mnt/shared/mesh-agg/surface.txt
 
-echo -e "#!/bin/bash\nOUTDIR=\$PWD\nVRIP_HOME=$BASEPATH/vrip\nexport VRIP_DIR=\$VRIP_HOME/src/vrip/\nPATH=\$PATH:\$VRIP_HOME/bin:$BASEPATH/tridecimator\ncd $SUBVOLDIR\n" > runvrip.sh
+echo -e "#!/bin/bash\nOUTDIR=\$PWD\nVRIP_HOME=$BASEPATH/vrip\nexport VRIP_DIR=\$VRIP_HOME/src/vrip/\nPATH=\$PATH:\$VRIP_HOME/bin:$BASEPATH/tridecimator\ncd $MESHCACHE\n" > runvrip.sh
 
-echo -e "$BASEPATH/vrip/bin/pvrip1 auto.vri \$OUTDIR/mesh-agg/total.ply surface.conf surface.conf  0.033 1000M ~/loadlimit -logdir /mnt/shared/log -rampscale 300 -subvoldir $SUBVOLDIR -nocrunch -passtovrip -use_bigger_bbox -dec\n" >> runvrip.sh
+echo -e "$BASEPATH/vrip/bin/pvrip1 auto.vri \$OUTDIR/mesh-agg/total.ply surface.txt surface.txt  0.033 1000M ~/loadlimit -logdir /mnt/shared/log -rampscale 300 -subvoldir $SUBVOLDIR -nocrunch -passtovrip -use_bigger_bbox -dec -meshcache $MESHCACHE\n" >> runvrip.sh
 chmod 0777  runvrip.sh 
 ./runvrip.sh
 

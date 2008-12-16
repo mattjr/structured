d41c610f305b3a35dee13aa6535b789fba3192b6
AUV <auv@acfr.usyd.edu.au>
2008-12-16 00:54:13 +0000

    This commit was generated by cvs2svn to compensate for changes in r3738, which included commits to RCS files with non-trunk default branches.

---
+++ quadtree/fast/Mytree.h
@@ -0,0 +1,61 @@
+/*
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish and distribute.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+  AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+  Copyright 1999 -   Laurent Balmelli
+                     Laboratory For Audio-Visual Communications 
+		     Ecole Polytechnique Federale de Lausanne, Switzerland
+
+		     also:
+		     Mathematics for Communication, Bell Laboratories
+		     Lucent Technologies, USA
+
+  contact, bugs:     Laurent.Balmelli@epfl.ch
+                     balmelli@acm.org
+		     
+  PLEASE DO NOT REMOVE THIS COPYRIGHT NOTICE. 
+		     
+ */
+
+#ifndef Mytree_h  
+#define Mytree_h       1
+
+#include "Quadtree.h"
+#include "Mynode.h"
+#include <stdio.h>
+
+class Mytree : public Quadtree<Mynode> {
+ 
+ public:
+
+  Mytree( int depth );
+  ~Mytree();
+
+  inline void myTraversal(int index) {
+
+    int coords[2],
+        current,
+        level;
+
+    current = getTRChildNode(index);
+    current = getNeighbor(current,_EAST);
+    printf("@ node %d\n",current);
+    current = getNeighbor(current,_NORTH);
+    printf("@ node %d\n",current);
+    current = getNeighbor(current,_NW);
+    printf("@ node %d\n",current);
+
+  }
+};
+
+#endif
---
+++ quadtree/fast/Quadnode.h
@@ -0,0 +1,44 @@
+/*
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish and distribute.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+  AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+  AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+  Copyright 1999 -   Laurent Balmelli
+                     Laboratory For Audio-Visual Communications 
+		     Ecole Polytechnique Federale de Lausanne, Switzerland
+
+		     also:
+		     Mathematics for Communication, Bell Laboratories
+		     Lucent Technologies, USA
+
+  contact, bugs:     Laurent.Balmelli@epfl.ch
+                     balmelli@research.bell-labs.com
+		     
+  PLEASE DO NOT REMOVE THIS COPYRIGHT NOTICE. 
+		     
+ */
+
+#ifndef Quadnode_h  
+#define Quadnode_h       1
+
+class Quadnode {
+
+ public:
+
+  Quadnode();
+  ~Quadnode();
+
+  short Coords[2]; 
+
+};
+ 
+#endif

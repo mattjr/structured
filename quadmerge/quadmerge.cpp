03ead4bae30fe5f527634a2330a0728dfa0cb5ca
Matthew Johnson-Roberson <m.roberson@acfr.usyd.edu.au>
2008-12-19 06:10:14 +0000

    slow memory intesive version

--- quadmerge/quadmerge.cpp
+++ quadmerge/quadmerge.cpp
@@ -23,39 +23,41 @@ int main( int argc, char **argv ) {
 	  }
 	  cnt++;
 	}
+	printf("Number of Pts %d\n",cnt);
 	fseek(fp,SEEK_SET,0);
-	float pts[cnt*DIMENSION];
-	float norms[cnt*DIMENSION];
-	int idx=0;
+	terrain_data *pts = new terrain_data[cnt*DIMENSION];
+      
+	//float *norms= new terrain_data[cnt*DIMENSION];
+	int ptcount=0;
 	while(1){
 	  if(fread(c,sizeof(float),2*DIMENSION,fp)!=6){break;}
-	  memcpy(&pts[idx],c,sizeof(float)*3);
-	  memcpy(&norms[idx],c,sizeof(float)*3);
-	  idx+=DIMENSION;
-	  
+	  pts[ptcount].x=(double)((float *)c)[0];
+	  pts[ptcount].y=(double)((float *)c)[1];
+	  pts[ptcount].z=(double)((float *)c)[2];
+	  ptcount++;
 	}
 	fclose(fp);
-	printf("%d count %f %f %f %f %f %f\n",cnt,min[0],min[1],min[2],max[0],max[1],max[2] );
+	printf("%d count %f %f %f %f %f %f\n",ptcount,min[0],min[1],min[2],max[0],max[1],max[2] );
 	mapnik::Envelope<double> tree_bounds(min[0],min[1],max[0],max[1]);
-	terrain_tree qt(tree_bounds,atoi(argv[2]),0.5);
-	for(i=0; i < cnt*DIMENSION; i+=DIMENSION){
-	  //	  cout << "pt : " << pts[i] << " " << pts[i+1] << " "<< pts[i +2]<<endl;
-	  Envelope<double> pt_ext((double)pts[i+0],(double)pts[i+1],(double)pts[i+0],(double)pts[i+1]);
-	  terrain_data data;
-	  data.x=pts[i];
-	  data.y=pts[i+1];
-	  data.z=pts[i+2];
+	terrain_tree qt(tree_bounds,20,0.5,atof(argv[2]));
+	for(i=0; i < ptcount; i++){
 
-	  qt.insert(data,pt_ext);
+	  Envelope<double> pt_ext(pts[i].x,pts[i].y,pts[i].x,pts[i].y);
+	  qt.insert(pts[i],pt_ext);
+
 	  //	  cout <<tree_bounds.contains(pt_ext)<< " "<< pt_ext << " total " << tree_bounds<<endl;
 	}
+
+	  delete pts;
+	  //delete norms;
 	//	qt.print();
 	//qt.draw();
 	printf("Num items %d\n",qt.count_items());
 // construct the viewer.
 //	qt.trim();
-	qt.balance();
-	qt.render_tree();
+//	qt.balance();
+	qt.render_terrain();
+	//qt.render_tree();
     osgViewer::Viewer viewer;
     viewer.setSceneData(qt.osg_root);
     return viewer.run();
--- quadmerge/terrainnode.hpp
+++ quadmerge/terrainnode.hpp
@@ -10,6 +10,8 @@
 #include <osg/Geode>
 #include <osg/Geometry>
 #include <osg/PolygonMode>
+#include <osgUtil/DelaunayTriangulator>
+#include <osg/Timer>
  class terrain_data{
 public:
    double x,y,z;
@@ -22,14 +24,16 @@ friend  std::ostream& operator << (std::ostream& os, const terrain_data &data)
 typedef quadtree_node<terrain_data> terrain_node;
 class terrain_tree:public quadtree<terrain_data>{
 public:
-  terrain_tree(const Envelope<double>& extent,int maxdepth,double ratio):quadtree<terrain_data>(extent,maxdepth,ratio)
+  terrain_tree(const Envelope<double>& extent,int maxdepth,double ratio,double min_extent_size):quadtree<terrain_data>(extent,maxdepth,ratio),min_extent_size_(min_extent_size)
   {
     osg_root = new osg::Group();
     tree_geode =new osg::Geode();
     tree_box_geometry = new osg::Geometry();
     tree_point_geometry = new osg::Geometry();
+    terrain_geometry = new osg::Geometry();
     tree_geode->addDrawable(tree_point_geometry); 
     tree_geode->addDrawable(tree_box_geometry); 
+    tree_geode->addDrawable(terrain_geometry); 
     osg_root->addChild(tree_geode);
     osg::StateSet *state = osg_root->getOrCreateStateSet();
     osg::PolygonMode *polyModeObj;
@@ -40,8 +44,10 @@ public:
     }
     polyModeObj->setMode(  osg::PolygonMode::FRONT_AND_BACK, osg::PolygonMode::LINE );
     
+
  
     tree_box_vertices=  new osg::Vec3Array;
+    terrain_vertices=  new osg::Vec3Array;
     tree_point_vertices=  new osg::Vec3Array;
 #ifdef PLOTTING
     pls = new plstream();
@@ -78,7 +84,54 @@ argv[0]= "quadmerge" ;
 }
 
 #ifdef PLOTTING
+   void insert(const terrain_data& data,const Envelope<double>& item_ext)
+    {
+        insert(data,item_ext,root_);
+    }
+  
+ void insert(const terrain_data data,const Envelope<double>& item_ext,quadtree_node<terrain_data>*  node )
+    {
+        if (node && node->ext_.contains(item_ext))
+        {
+            coord2d c=node->ext_.center();
 
+            double width=node->ext_.width();
+            double height=node->ext_.height();
+
+            double lox=node->ext_.minx();
+            double loy=node->ext_.miny();
+            double hix=node->ext_.maxx();
+            double hiy=node->ext_.maxy();
+
+            Envelope<double> ext[4];
+            ext[0]=Envelope<double>(lox,loy,lox + width * ratio_,loy + height * ratio_);
+            ext[1]=Envelope<double>(hix - width * ratio_,loy,hix,loy + height * ratio_);
+            ext[2]=Envelope<double>(lox,hiy - height*ratio_,lox + width * ratio_,hiy);
+            ext[3]=Envelope<double>(hix - width * ratio_,hiy - height*ratio_,hix,hiy);
+
+            if (width <= _min_extent_size || height <= _min_extent_size)
+            {
+                for (int i=0;i<4;++i)
+                {
+                   
+                        if (!node->children_[i])
+			{
+                            node->children_[i]=new quadtree_node<T>(ext[i]);
+                        }
+
+		}
+		for(int i=0; i<4; ++i){
+		  if (ext[i].contains(item_ext)) {
+		    insert(data,item_ext,node->children_[i]);
+		    return;
+		  }
+		  
+		}
+
+            }
+            node->data_.push_back(data);
+        }
+    }
  void draw() const
     {
       pls->init();
@@ -117,8 +170,7 @@ argv[0]= "quadmerge" ;
         }
     }
 #endif
-void render_tree() const
-    {
+  void render_tree() const {
     
 		render_tree(root_);
 		//	osg::DrawElementsUInt* tree_base = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, tree_box_vertices->size());
@@ -147,16 +199,73 @@ void render_tree() const
       }
     }
   }
+
+
+
+  void render_terrain() const {
+    
+		render_terrain(root_);
+		//	osg::DrawElementsUInt* tree_base = new osg::DrawElementsUInt(osg::PrimitiveSet::QUADS, tree_box_vertices->size());
+		// create triangulator and set the points as the area
+		osg::ref_ptr<osgUtil::DelaunayTriangulator> trig = new osgUtil::DelaunayTriangulator();
+		trig->setInputPointArray(terrain_vertices);
+		printf("Triangulating %d pts...\n",terrain_vertices->size());
+		const osg::Timer& timer = *osg::Timer::instance();
+		osg::Timer_t start_tick = timer.tick();
+		
+	   
+		trig->triangulate();
+		         
+		double duration = timer.delta_s(start_tick,timer.tick());
+		printf("Elapsed time for Triangulate %f\n",duration);
+		terrain_geometry->setVertexArray(terrain_vertices);
+		terrain_geometry->addPrimitiveSet(trig->getTriangles());
+
+ 
+		//		terrain_geometry->setVertexArray(terrain_vertices);
+		// 	terrain_geometry->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::TRIANGLES,0,terrain_vertices->size()));
+		
+    }
+osg::Vec3 get_estimated_pos(const std::vector<terrain_data> &data) const{
+  osg::Vec3 sum;
+    unsigned int i=0;
+    if(data.size() ==0)
+      return osg::Vec3(0,0,0);
+    for(i=0; i < data.size(); i++){
+      sum+=      osg::Vec3(data[i].x,data[i].y,data[i].z);
+    }
+    sum[0]/=(double)i;
+    sum[1]/=(double)i;
+    sum[2]/=(double)i;
+    return sum;
+
+  }
+  void render_terrain(const terrain_node *node) const{
+    
+ 
+    
+    if (node ){
+      if(node->children_[0] == NULL && node->data_.size()){
+
+	terrain_vertices->push_back( get_estimated_pos(node->data_));
+      }
+    
+      for (int i=0;i<4;++i){
+	render_terrain(node->children_[i]);
+      }
+    }
+  }
 private:
 #ifdef PLOTTING
    plstream *pls;
 const char *f_name;
 #endif
-
+  double zmin,zmax,min_extent_size_;
 osg::Geode* tree_geode;
 osg::Geometry *tree_box_geometry;
 osg::Geometry *tree_point_geometry;
-
+osg::Geometry *terrain_geometry;
+osg::Vec3Array* terrain_vertices;
 osg::Vec3Array* tree_box_vertices;
 osg::Vec3Array* tree_point_vertices;
 public :

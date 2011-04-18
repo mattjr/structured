255d5c95d8a21e22d2da691afb9d65ae66c4978f
AUV <auv@acfr.usyd.edu.au>
2011-04-18 23:28:56 +0000

    meshmerge clean works

--- texturedDecimator/mergeMesh.cpp
+++ texturedDecimator/mergeMesh.cpp
@@ -63,17 +63,7 @@ int main(int argc ,char**argv){
  /*   vcg::tri::UpdateTopology<CMeshO>::FaceFace(cm);
      vcg::tri::UpdateFlags<CMeshO>::FaceBorderFromFF(cm);
     vcg::tri::UpdateNormals<CMeshO>::PerVertexNormalized(cm);*/
-    if(clean){
-        cm.face.EnableFFAdjacency();
-          cm.face.EnableMark();
-          tri::UpdateTopology<CMeshO>::FaceFace(cm);
-          tri::UpdateFlags<CMeshO>::FaceBorderFromFF(cm);
 
-        float minCC= CCPerc*cm.bbox.Diag();
-        printf("Cleaning Min CC %f\n",minCC);
-           std::pair<int,int> delInfo= tri::Clean<CMeshO>::RemoveSmallConnectedComponentsDiameter(cm,minCC);
-           printf("Removed %d/%d\n",delInfo.first,delInfo.second);
-    }
     if(err) {
         fprintf(stderr,"Unable to open mesh %s : '%s'\n",argv[1],vcg::tri::io::ImporterPLY<CMeshO>::ErrorMsg(err));
         exit(-1);
@@ -85,6 +75,22 @@ int main(int argc ,char**argv){
          tri::Clean<CMeshO>::FlipMesh(cm);
      }
 
+     if(clean){
+         tri::UpdateBounding<CMeshO>::Box(cm);
+         cm.face.EnableFFAdjacency();
+           cm.face.EnableMark();
+           tri::UpdateTopology<CMeshO>::FaceFace(cm);
+           tri::UpdateFlags<CMeshO>::FaceBorderFromFF(cm);
+
+         float minCC= CCPerc*cm.bbox.Diag();
+         printf("Cleaning Min CC %f\n",minCC);
+            std::pair<int,int> delInfo= tri::Clean<CMeshO>::RemoveSmallConnectedComponentsDiameter(cm,minCC);
+            printf("Removed %d/%d\n",delInfo.second,delInfo.first);
+            int dup= tri::Clean<CMeshO>::RemoveDuplicateVertex(cm);
+             int unref= tri::Clean<CMeshO>::RemoveUnreferencedVertex(cm);
+             int deg= vcg::tri::Clean<CMeshO>::RemoveDegenerateFace(cm);
+
+     }
     bool binaryFlag =true;
 
     vcg::tri::io::PlyInfo pi;

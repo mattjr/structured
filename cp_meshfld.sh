d63ad44b7891b4c054afad9fc57add5e3b16d07b
mattjr <mattjr@acfr.usyd.edu.au>
2012-11-16 10:00:54 +1100

    added copy meshfld

---
+++ cp_meshfld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+if [ ! -d "$1" ]; then
+    echo "$1 Does not exist bailing"
+    exit -1
+fi
+if [ ! -d "$2" ]; then
+    echo "$2 Does not exist creating"
+    mkdir $2
+fi
+args="-p -d"
+FILES=("stereo_pose_est.data" "localiser.cfg" "stereo.calib" "img")
+for f in "${FILES[@]}"
+do
+    echo "Copying $f"
+    cp $args $1/$f $2/$f
+
+done
+echo "Copying mesh.cfg"
+cp ~/git/structured-vt-reorder/mesh.cfg $2/

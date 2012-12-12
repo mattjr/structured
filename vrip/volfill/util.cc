93354889f3f0a8c83c86d1e3796d29957423e4e4
mattjr <mattjr@acfr.usyd.edu.au>
2012-12-12 17:09:36 +1100

    volfill added

---
+++ vrip/volfill/util.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <unistd.h>
+
+void PrintResourceUsage(const char *msg) {
+
+    std::cerr << "volfill info: " << msg << ":";
+	
+	struct rusage ru;
+	getrusage(RUSAGE_SELF, &ru);
+	char buf[100];
+	sprintf(buf, " user %d.%.1ds system %d.%.1ds", ru.ru_utime.tv_sec,
+					ru.ru_utime.tv_usec / 100000, ru.ru_stime.tv_sec, 
+					ru.ru_stime.tv_usec / 100000);
+	
+#ifdef linux   
+	std::cerr << buf;
+	sprintf(buf, "ps h -o vsize -p %d", getpid());
+	FILE *p = popen(buf, "r");
+	int vsize;
+	fscanf(p, "%d", &vsize);
+	fclose(p);
+	sprintf(buf, " vsize %.2fMB", vsize / 1024.0);
+#endif // linux   
+
+	std::cerr << buf << std::endl;
+}

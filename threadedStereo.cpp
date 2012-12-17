998204c921a491a1b7819449d0762d1658eb4d84
mattjr <mattjr@acfr.usyd.edu.au>
2012-12-17 18:14:11 +1100

    fix thumb bug

--- threadedStereo.cpp
+++ threadedStereo.cpp
@@ -2512,7 +2512,7 @@ double totalValidArea=0;
         fprintf(stderr,"Can't open mosaic scripts\n");
         exit(-1);
     }
-    fprintf(FP3,"#!/bin/bash\n cd mosaic \n gdalbuildvrt  mosaic.vrt; rm -f thumb.tif; gdalwarp mosaic.vrt -ts 512 512 thumb.tif;convert thumb.tif thumb.png\n");
+    fprintf(FP3,"#!/bin/bash\n cd mosaic \n gdalbuildvrt  mosaic.vrt ");
     fprintf(FP4,"#!/bin/bash\n cd mosaic \n gdalbuildvrt  mosaicvar.vrt ");
     fprintf(FP5,"#!/bin/bash\n cd mosaic \n gdalbuildvrt  depth.vrt ");
     for(int i=0; i <(int)cells_mosaic.size(); i++){
@@ -2685,7 +2685,8 @@ double totalValidArea=0;
     fclose(vartexcmds_fp);
 
     fclose(calcTexFn_fp);
-    fprintf(FP3,"\n#gdaladdo -ro --config INTERLEAVE_OVERVIEW PIXEL --config COMPRESS_OVERVIEW JPEG mosaic.vrt 2 4 8 16 32\n");
+    fprintf(FP3,"\nrm -f thumb.tif\ngdalwarp mosaic.vrt -ts 512 512 thumb.tif\nconvert thumb.tif thumb.png\n");
+        //#gdaladdo -ro --config INTERLEAVE_OVERVIEW PIXEL --config COMPRESS_OVERVIEW JPEG mosaic.vrt 2 4 8 16 32\n");
     fchmod(fileno(FP3),0777);
 
     fclose(FP3);

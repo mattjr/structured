81b7fe8ed52f3cfc30b2e4b7ef0855829203b3af
mattjr <mattjr@abyss.auv.acfr.usyd.edu.au>
2013-01-24 17:56:18 +1100

    old vips func

--- im_shrink_noblack.cpp
+++ im_shrink_noblack.cpp
@@ -194,7 +194,7 @@ shrink_noblack( IMAGE *in, IMAGE *out, double xshrink, double yshrink )
 
         /* Check parameters.
          */
-        if( !in || vips_bandfmt_iscomplex( in->BandFmt ) ) {
+        if( !in /*|| vips_bandfmt_iscomplex( in->BandFmt )*/ ) {
                 im_error( "im_shrink", "%s", _( "non-complex input only" ) );
                 return( -1 );
         }

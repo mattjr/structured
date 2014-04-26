7e962306f0f0b5812145a2c6de662468bc11ae0e
Matthew Johnson-Roberson <mattkjr@gmail.com>
2014-04-27 03:22:25 +1000

    working tiling

--- gen_j2k.py
+++ gen_j2k.py
@@ -5,8 +5,47 @@ import os
 import multiprocessing
 import tarfile
 import subprocess,shlex
+from os import kill
+from signal import alarm, signal, SIGALRM, SIGKILL
+from subprocess import PIPE, Popen
 from PIL import Image
 black = (0,0,0)
+def run(args, cwd = None, shell = False, kill_tree = True, timeout = -1, env = None):
+    '''
+    Run a command with a timeout after which it will be forcibly
+    killed.
+    '''
+    class Alarm(Exception):
+        pass
+    def alarm_handler(signum, frame):
+        raise Alarm
+    p = Popen(args, shell = shell, cwd = cwd, stdout = PIPE, stderr = PIPE, env = env)
+    if timeout != -1:
+        signal(SIGALRM, alarm_handler)
+        alarm(timeout)
+    try:
+        stdout, stderr = p.communicate()
+        if timeout != -1:
+            alarm(0)
+    except Alarm:
+        pids = [p.pid]
+        if kill_tree:
+            pids.extend(get_process_children(p.pid))
+        for pid in pids:
+            # process might have died before getting to this line
+            # so wrap to avoid OSError: no such process
+            try: 
+                kill(pid, SIGKILL)
+            except OSError:
+                pass
+        return -9, '', ''
+    return p.returncode, stdout, stderr
+
+def get_process_children(pid):
+    p = Popen('ps --no-headers -o pid --ppid %d' % pid, shell = True,
+              stdout = PIPE, stderr = PIPE)
+    stdout, stderr = p.communicate()
+    return [int(p) for p in stdout.split()]
 
 class Curr(ProgressBarWidget):
     "Just the percentage done."
@@ -25,7 +64,10 @@ if len(sys.argv) < 4:
     threads=multiprocessing.cpu_count()
 else:
     threads = int(sys.argv[3])
-
+clampth=8
+if threads > clampth:
+    print 'seems to deadlock over small number of threads clamping to %d' %clampth
+    threads = clampth
 outfile=sys.argv[2]
 if os.path.isfile(outfile):
     os.remove(outfile)
@@ -69,7 +111,8 @@ with open(mosaic_txt) as f:
         pairs.add((i,j))
         fname_dict[(i,j)]=filename
 #filename=os.path.realpath(os.getcwd()+'/mosaic/image_r%04d_c%04d_rs%04d_cs%04d.tif' % (i,j,rows,cols))
-for i in range(0,rows):
+#need to flip i from top to bottom for coord system used in image
+for i in range(rows-1,-1,-1):
     for j in range(0,cols):
         if (i,j) not in pairs:
             #print filename
@@ -82,15 +125,22 @@ for i in range(0,rows):
             print 'Error file missing "%s"' % filename
         
 
-        cmd='%s/kd/kdu_compress -i %s -o %s  -quiet %s -frag %d,%d,1,1 -num_threads %d' % (pathname,filename,outfile,args,i,j,threads)
+        cmd='%s/kd/kdu_compress -i %s -o %s  -quiet %s -frag %d,%d,1,1 -num_threads %d' % (pathname,filename,outfile,args,(rows-i-1),j,threads)
         #print cmd
         parsedcmd=shlex.split(cmd)
-        child = subprocess.Popen(parsedcmd, env=myenv,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        streamdata = child.communicate()[0]
-        rc = child.returncode
+        #child = subprocess.Popen(parsedcmd, env=myenv,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        #streamdata = child.communicate()[0]
+        #rc = child.returncode
+        retry = 0
+        rc, stdouttxt, stderrtxt = run(parsedcmd, env=myenv, timeout = 120)
+        while rc == -9 and retry < 3:
+            print 'killed after deadlock retrying'
+            rc, stdouttxt, stderrtxt = run(parsedcmd, env=myenv, timeout = 120)
+            retry=retry+1
+
         if rc < 0:
             print 'Failed to execute command'
-            print streamdata
+            print stdouttxt,stderrtxt
         pbar.inc()
        # print '%04d/%04d of %d' %(valid,invalid,total)
         
